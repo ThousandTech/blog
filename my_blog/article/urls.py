50631b627a840cda43e47fd27c7a581f65b592d0
ThousandTech <twq15524046067@126.com>
2025-12-31 16:32:34 +0800

    special:具体更改见上条,停止跟踪缓存导致分两次提交,本提交与提交7cd6c83202f1dd00b2738f115eade1769e21f496视为同一次提交

---
+++ my_blog/article/urls.py
@@ -0,0 +1,6 @@
+# 03article 自己的路由分发文件
+from django.urls import path# 03引入path函数
+
+app_name = 'article'# 03设置命名空间
+
+urlpatterns = []# 03定义一个空的路径映射列表
---
+++ my_blog/my_blog/urls.py
@@ -0,0 +1,26 @@
+"""my_blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include# 03引入include函数
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('article/', include('article.urls', namespace='article')),
+    # 03告诉django怎样定位article应用
+    # 03 'article/'参数分配了一个URL前缀给article应用
+    # 03 include('article.urls', namespace='article')参数告诉Django去查找article应用下的urls.py文件
+    # 03 namespace='article'参数为article应用指定了一个命名空间
+]
