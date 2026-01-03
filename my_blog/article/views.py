ae7575bb05b71dc3c7f5c937ce18ee359377752e
ThousandTech <twq15524046067@126.com>
2026-01-03 13:08:45 +0800

    add:新建模板文件夹并添加基础文章列表模板文件 update:更新article_list视图函数，传递所有博客文章 update:更改settings.py模板文件夹位置

--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 # 05 导入HttpResponse模块
 from django.http import HttpResponse
 
+# 06 导入数据模型
+from .models import ArticlePost
+
 # 05 视图函数，返回HttpResponse对象或者抛出异常，request参数与请求类型有关
 def article_list(request):
-    # 05 返回最简单的网页
-    return HttpResponse("Hello World!")
+    # # 05 返回最简单的网页
+    # return HttpResponse("Hello World!")
+    # 06 从数据库取出所有的博客文章
+    articles = ArticlePost.objects.all()
+    # 06 context字典定义了要传递给模板的上下文
+    context = {'articles':articles}
+    # 06 render函数结合模板与上下文并返回渲染后的HttpResponse对象
+    # 06 render的三个参数分别为固定的request，模板文件，传入模板文件的上下文（字典）
+    return render(request,'article/list.html',context)
