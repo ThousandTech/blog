765f9877207836e1eb5e1a2d7a1f583ab7bdc35b
ThousandTech <twq15524046067@126.com>
2026-01-03 17:02:03 +0800

    add:新建文章详情页 update:更新article app的urls.py，添加文章详情页路由 update:更新article app的views.py,增加文章详情视图函数 update:更新导航栏模板，建立与文章列表页的连接 update:更新文章列表页，建立与文章详情页的连接

--- my_blog/article/urls.py
+++ my_blog/article/urls.py
@@ -2,9 +2,11 @@
 from django.urls import path# 03引入path函数
 from . import views
 
-app_name = 'article'# 03设置命名空间
+app_name = 'article'# 03 设置命名空间
 
 urlpatterns = [
-    path('article-list',views.article_list,name='article_list')
     # 05 django根据url选择对应的视图函数，name相当于主键，使第一个参数即URL可以任意改变
+    path('article-list',views.article_list,name='article_list'),
+    # 08 定义带参数的URL，用尖括号定义需要传递的参数，int限定参数类型，将值保存在id中并传递给后面的视图函数
+    path('article-detail/<int:id>/',views.article_detail,name='article_detail')
 ]# 03定义一个空的路径映射列表
--- my_blog/article/views.py
+++ my_blog/article/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # 06 导入数据模型
 from .models import ArticlePost
 
-# 05 视图函数，返回HttpResponse对象或者抛出异常，request参数与请求类型有关
+# 05 主页视图函数，返回HttpResponse对象或者抛出异常，request参数与请求类型有关
 def article_list(request):
     # # 05 返回最简单的网页
     # return HttpResponse("Hello World!")
@@ -16,3 +16,12 @@ def article_list(request):
     # 06 render函数结合模板与上下文并返回渲染后的HttpResponse对象
     # 06 render的三个参数分别为固定的request，模板文件，传入模板文件的上下文（字典）
     return render(request,'article/list.html',context)
+
+# 08 文章详情视图函数，需要参数id以得到确切的文章
+def article_detail(request,id):
+
+    article = ArticlePost.objects.get(id=id)
+
+    context = {'article':article}
+
+    return render(request,'article/detail.html',context)
